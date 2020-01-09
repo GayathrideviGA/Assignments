d24814530e70c895b1fda778ba3dd31b297cdb4d
GayathrideviGA25 <gayathrideviga25@gmail.com>
2020-01-09 14:38:11 +0530

    Mode Swing using Toggle button

--- mode_swing/lib/main.dart
+++ mode_swing/lib/main.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
@@ -25,86 +24,123 @@ class MyHomepagestate extends State<MyHomepage> {
     style: TextStyle(
         fontSize: 20, fontWeight: FontWeight.bold, color: Colors.white),
   );
-  Text night = Text(
-    'Change to Day Mode',
-    textAlign: TextAlign.center,
-    style: TextStyle(
-        fontSize: 20, fontWeight: FontWeight.bold, color: Colors.white),
-  );
+
   Color dayappbg = Colors.yellow[800];
-  
-  Color nightappbg = Colors.deepPurple[900];
-  Color dbg = Colors.yellow[200];
-  Color nbg = Colors.deepPurple[800];
+
+  Color dbg = Colors.yellow[700];
+
   AssetImage dayimage = AssetImage('assets/day.png');
-  AssetImage nightimage = AssetImage('assets/night.png');
+
   Text daystate = Text(
     'Good Morning!!!',
-    textAlign: TextAlign.center,
-    style: TextStyle(
-        fontSize: 20, fontWeight: FontWeight.bold, color: Colors.yellow[800]),
-  );
-  Text nightstate = Text(
-    'Good Night!!!',
     textAlign: TextAlign.center,
     style: TextStyle(
         fontSize: 20, fontWeight: FontWeight.bold, color: Colors.white),
   );
 
-  @override
+  List<bool> isSelected = [true, false];
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        backgroundColor: dbg,
-        appBar: AppBar(
-          title: Center(
-            child: day,
-          ),
-          backgroundColor: dayappbg,
+    return Scaffold(
+      backgroundColor: dbg,
+      appBar: AppBar(
+        backgroundColor: dayappbg,
+        title: Center(
+          child: day
         ),
-        body: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: <Widget>[
-            Container(
-              margin: EdgeInsets.all(20),
-              width: 150,
-              height: 50,
-              decoration: BoxDecoration(
-                color: dayappbg,
-                borderRadius: BorderRadius.circular(10)
-              ),
-              alignment: Alignment.center,
-              child: FlatButton(
-             
-             onPressed: (){
-                setState(() {
-                  dayappbg = nightappbg;
-                  day = night;
-                  dbg = nbg;
-                  dayimage = nightimage;
-                  daystate = nightstate;
-                });
-             },
-             child: Text('Change Mode'),
-           ),
-            ),
-            Container(
-              alignment: Alignment.topCenter,
-              margin: EdgeInsets.all(20),
-              width: 150,
-              height: 150,
-              decoration: BoxDecoration(
-                image: DecorationImage(
-                  image: dayimage,
-                  fit: BoxFit.cover,
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Padding(
+                padding: EdgeInsets.only(top: 10),
+                child: ToggleButtons(
+                  selectedBorderColor: Colors.black,
+                  selectedColor: Colors.brown,
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(50),
+                  borderWidth: 2,
+                  borderColor: Colors.black,
+                  children: <Widget>[
+                    Icon(Icons.wb_sunny),
+                    Icon(
+                      Icons.wb_sunny,
+                      color: Colors.black,
+                    )
+                  ],
+                  isSelected: isSelected,
+                  onPressed: (int index) {
+                    setState(() {
+                      for (int buttonIndex = 0;
+                          buttonIndex < isSelected.length;
+                          buttonIndex++) {
+                        if (buttonIndex == index) {
+                          isSelected[buttonIndex] = !isSelected[buttonIndex];
+                          dayappbg = Colors.deepPurple[900];
+                          day = Text(
+                            'Change to Day Mode',
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                                fontSize: 20,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.white),
+                          );
+                          dayimage = AssetImage('assets/night.png');
+                          dbg = Colors.deepPurple[800];
+                          daystate = Text(
+                            'Good Night!!!',
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                                fontSize: 20,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.white),
+                          );
+                        } else {
+                          isSelected[buttonIndex] = false;
+                          day = Text(
+    'Change to Night Mode',
+    textAlign: TextAlign.center,
+    style: TextStyle(
+        fontSize: 20, fontWeight: FontWeight.bold, color: Colors.white),
+  );
+                          dayappbg = Colors.yellow[800];
+                          dayimage = AssetImage('assets/day.png');
+                          dbg = Colors.yellow[700];
+                          daystate = Text(
+    'Good Morning!!!',
+    textAlign: TextAlign.center,
+    style: TextStyle(
+        fontSize: 20, fontWeight: FontWeight.bold, color: Colors.white),
+  );
+                        }
+                      }
+                    });
+                  },
                 ),
               ),
-            ),
-            Center(
-              child: daystate,
-            )
-          ],
-        ),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Container(
+                alignment: Alignment.topCenter,
+                margin: EdgeInsets.all(20),
+                width: 150,
+                height: 150,
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    image: dayimage,
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+            ],
+          ),
+          daystate
+        ],
       ),
     );
   }
